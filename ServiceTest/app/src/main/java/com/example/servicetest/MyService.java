85a21cae7cc21332fcd7ff565d0a58f208213af0
xinyunsong <xinyunsong@microsoft.com>
2022-11-30 18:00:02 +0800

    add foreground service (need to add channel)

--- ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
+++ ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -1,10 +1,16 @@
 package com.example.servicetest;
 
+import android.Manifest;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
 
@@ -32,6 +38,18 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d("MyService", "onCreate");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("title")
+                .setContentText("text")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentIntent(pi)
+                .build();
+        startForeground(1, notification);
     }
 
     @Override
