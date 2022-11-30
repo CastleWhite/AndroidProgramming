cdfe18bc49c55363b6a0453ff9f43a0f3cc06e19
xinyunsong <xinyunsong@microsoft.com>
2022-11-30 17:42:32 +0800

    add service test

---
+++ ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.servicetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private MyService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            downloadBinder = (MyService.DownloadBinder) iBinder;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button start = (Button) findViewById(R.id.button);
+        Button stop = (Button) findViewById(R.id.button2);
+        start.setOnClickListener(this);
+        stop.setOnClickListener(this);
+
+        Button bind = (Button) findViewById(R.id.button3);
+        Button unbind = (Button) findViewById(R.id.button4);
+        bind.setOnClickListener(this);
+        unbind.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                Intent startIntent = new Intent(this, MyService.class);
+                startService(startIntent);
+                break;
+            case R.id.button2:
+                Intent stopIntent = new Intent(this, MyService.class);
+                stopService(stopIntent);
+                break;
+            case R.id.button3:
+                Intent bindIntent = new Intent(this, MyService.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE);
+                break;
+            case R.id.button4:
+                unbindService(connection);
+                break;
+            default:
+                break;
+        }
+    }
+
+}
---
+++ ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -0,0 +1,48 @@
+package com.example.servicetest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder {
+        public void startDownload() {
+            Log.d("Binder", "start down");
+        }
+        public int getProgress() {
+            Log.d("Binder", "get progress");
+            return 0;
+        }
+    }
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("MyService", "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService", "onStart");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MyService", "onDestroy");
+    }
+}
